4f605fc40fe8f3c217873cbcfd0db0ede7050f4d
Cpoet55 <2577383178@qq.com>
2020-04-09 10:30:20 +0800

    提交代码

---
+++ 扫雷/扫雷/game.h
@@ -0,0 +1,15 @@
+#ifndef __GAME_H__
+#define __GAME_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+#define ROW 12
+#define COL 12
+#define COUNT 10
+
+extern char show_mine[ROW][COL]
+#endif;
